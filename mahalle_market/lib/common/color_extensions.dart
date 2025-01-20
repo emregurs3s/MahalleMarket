7387686a23ffec5c640a48df70972fbc9ee45345
Emre Gürses <emregurses06@gmail.com>
2025-01-20 22:23:06 +0300

    common widget kısmına round text field eklendi ve navigatörlerle sayfalar birbirine bağlandı

--- mahalle_market/lib/common/color_extensions.dart
+++ mahalle_market/lib/common/color_extensions.dart
@@ -1,9 +1,1 @@
-import 'package:flutter/material.dart';
 
-class TColor {
-  static Color get primary => const Color(0xFFC6FF00);
-  static Color get primaryText => const Color(0xFF00000);
-  static Color get SecondaryText => const Color(0xFF303030);
-  static Color get textfield => const Color(0xFFD6D6D6);
-  static Color get placeholder => const Color(0xFF9E9E9E);
-}
