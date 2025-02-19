f4cf989bbf27a24b5a2c132e3e02868d73906122
Emre Gürses <emregurses06@gmail.com>
2025-02-19 23:26:30 +0300

    main_tabviewe eklemeler yapıldı

--- mahalle_market/lib/view/main_tabview/main_tabview.dart
+++ mahalle_market/lib/view/main_tabview/main_tabview.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:mahalle_market/common/color_extensions.dart';
+import 'package:mahalle_market/view/home/home_view.dart';
 
 class MainTabview extends StatefulWidget {
   const MainTabview({super.key});
@@ -8,11 +10,24 @@ class MainTabview extends StatefulWidget {
 }
 
 class _MainTabviewState extends State<MainTabview> {
+  int selctTab = 2;
+  PageStorageBucket storageBucket = PageStorageBucket();
+  Widget selectPageView = const HomeView();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: PageStorage(bucket: storageBucket, child: selectPageView),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          if (selctTab != 2) {
+            selctTab = 2;
+            selectPageView = HomeView();
+          } else {}
+          if (mounted) {
+            setState(() {});
+          }
+        },
+        backgroundColor: selctTab == 2 ? TColor.primary : TColor.placeholder,
         child: Image.asset(
           "mahalle_market/assets/img/home.png",
           width: 35,
